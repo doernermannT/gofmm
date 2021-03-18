e7e893ff4a577bff6e6b165c0502fb2ecd5989ea
Tianyi Ge <getianyi602@gmail.com>
2021-03-19 00:15:38 +0100

    new

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@
 
 from distutils.core import setup
 setup(
-  name = 'gofmm',         # How you named your package folder (MyLib)
-  packages = ['gofmm'],   # Chose the same as "name"
+  name = 'gofmm1',         # How you named your package folder (MyLib)
+  packages = ['gofmm1'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
   author = 'YOUR NAME',                   # Type in your name
   author_email = 'your.email@domain.com',      # Type in your E-Mail
   url = 'https://github.com/doernermannT/gofmm',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/doernermannT/gofmm/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/doernermannT/gofmm/archive/v_02.tar.gz',    # I explain this later on
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'validators',
